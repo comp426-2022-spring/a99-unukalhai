a7a3405fd5f2dffd8b8e7c3f84449e515481a5be
Madyson Barber <madysonb@live.unc.edu>
2022-03-27 15:31:11 -0400

    init routes folder and files

---
+++ tracker/routes/users.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+// needed routes: retrieve, add, update, delete
+
+
+// retrieve user
+
+
+// add user
+
+
+// update user
+
+
+// delete user
+
+
+
+
+module.exports = router;
