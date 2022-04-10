af8442518ceffc03f0e251adbf5072831843e4c3
Maytee Akkaramongkolrojn <77748481+MayteeAkk@users.noreply.github.com>
2022-04-10 14:28:19 -0400

    Added a Simple Dashboard

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "@progress/kendo-theme-material/dist/all.css";
+import { TileLayout } from "@progress/kendo-react-layout";
+import { useState } from "react";
+import "./App.css";
+
+const FirstWidget = () => <div> First Widget</div>;
+const SecondWidget = () => <div> Second Widget</div>;
+
+const initPositions = [
+  {
+    col:1,
+    colSpan:2,
+    rowSpan:2,
+  },
+  {
+    col:3,
+    colSpan:1,
+    rowSpan:1,
+  },
+];
 
 function App() {
+  const [positions, setPositions] = useState(initPositions);
+
+  const widgets = [
+    {
+      header: "First Widget Header",
+      body: <FirstWidget />,
+    },
+    {
+      header: "Second Widget Header",
+      body: <SecondWidget />,
+    },
+  ];
+
+  const handleRepositioning = (newPositions) => {
+    setPositions(newPositions.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Resource Dashboard</h1>
+      <TileLayout
+        className="tile-layout"
+        columns = {4}
+        rowHeight = {200}
+        gap ={{rows:10, columns:10}}
+        positions = {positions}
+        items = {widgets}
+        onRepositioning = {handleRepositioning}
+      />
     </div>
   );
+  // return <div className="App"></div>;
 }
 
 export default App;
