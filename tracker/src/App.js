85a826baabca5452e451ce25123388b6a2e699e7
Maytee Akkaramongkolrojn <77748481+MayteeAkk@users.noreply.github.com>
2022-04-10 14:34:32 -0400

    added some comments for code readability

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -3,10 +3,12 @@ import { TileLayout } from "@progress/kendo-react-layout";
 import { useState } from "react";
 import "./App.css";
 
+//These are the widget objects that are displayed in the dashboard
 const FirstWidget = () => <div> First Widget</div>;
 const SecondWidget = () => <div> Second Widget</div>;
 
 const initPositions = [
+  //This is to initalize where the widgets are places when server starts
   {
     col:1,
     colSpan:2,
@@ -32,11 +34,11 @@ function App() {
       body: <SecondWidget />,
     },
   ];
-
+  //This function allows for the widgets to be moved
   const handleRepositioning = (newPositions) => {
     setPositions(newPositions.value);
   };
-
+  //This returns the widgets to be displayed in the dashboard
   return (
     <div className="App">
       <h1>Resource Dashboard</h1>
