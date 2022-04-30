6350908334645154f506911dde9de844cc88ab10
Meghan Sun <meghansun322@gmail.com>
2022-04-29 20:09:41 -0400

    added resources data

--- frontend/data/data.js
+++ frontend/data/data.js
@@ -1,47 +1,123 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default [
-    {
-      id: 1,
-      title: "Parthenon",
-      location: "GREECE",
-      googleMapsUrl: "https://goo.gl/maps/a2UaeW8LQQ6RvxD3A",
-      natural: false,
-      dateBuilt: "438 BC",
-      description:
-        "The Parthenon is a former temple on the Athenian Acropolis, regarded as an enduring symbol of Ancient Greece, democracy and Western civilization, and one of the world's greatest cultural monuments.",
-      imageUrl: "https://source.unsplash.com/ilQmlVIMN4c",
-    },
-    {
-      id: 2,
-      title: "Mount Fuji",
-      location: "Japan",
-      googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-      natural: true,
-      dateBuilt: "",
-      description:
-        "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-      imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
-    },
-    {
-      id: 3,
-      title: "Sydney Opera House",
-      location: "Australia",
-      googleMapsUrl: "https://goo.gl/maps/bvUa3DjYZLp5BeaD6",
-      natural: false,
-      dateBuilt: "1973",
-      description:
-        "The Sydney Opera House is a multi-venue performing arts centre in Sydney. It is often regarded as one of the 20th century's most famous and distinctive buildings",
-      imageUrl: "https://source.unsplash.com/JmuyB_LibRo",
-    },
-    {
-      id: 4,
-      title: "Geirangerfjord",
-      location: "Norway",
-      googleMapsUrl: "https://goo.gl/maps/MLVd3w4bAxujF4mG7",
-      natural: true,
-      dateBuilt: "",
-      description:
-        "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality. The fjord is one of Norway's most visited tourist sites.",
-      imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
-    },
-  ];
+  {
+    id: 1,
+    name: "911 - Emergency",
+    description:
+      "For any situation requiring immediate assistance from the police fire department or ambulance dial 911 for emergency response",
+    keywords: "Emergency, Professional Care",
+    location: "828 M.L.K. Jr Blvd, Chapel Hill, NC 27514",
+    phoneNumber: "911",
+    website: "https://www.911.gov",
+    image:
+      "https://campushealth.unc.edu/wp-content/uploads/2021/09/UNC-EMS-882x1024.png",
+  },
+  {
+    id: 2,
+    name: "Active Minds at Carolina",
+    description:
+      "Focused on increasing awareness of mental health conditions, providing information and resources, and encourage help-seeking",
+    website: "https://heellife.unc.edu/organization/active-minds",
+    phoneNumber: "null",
+    location: "209 South Rd, Chapel Hill, NC 27599",
+    keywords: "Community Support, Information",
+    image:
+      "https://se-images.campuslabs.com/clink/images/074a951c-704c-4b35-9e81-f16da39f9f3ed291f0dd-d7c7-47c5-9ba9-e014a2a1dc04.jpg?preset=med-sq",
+  },
+  {
+    id: 3,
+    name: "Actively Moving Forward",
+    description:
+      "A peer-led grief support community for students who have experienced the death or terminal illness of a loved one.",
+    website: "https://heellife.unc.edu/organization/amf",
+    phoneNumber: "null",
+    location: "209 South Rd, Chapel Hill, NC 27599",
+    keywords: "Peer Support, Grief",
+    image:
+      "https://localist-images.azureedge.net/photos/860379/big_square@2x/9de0e4b22e4b4d411b1f00311d8e0eaaedc5f4e8.jpg",
+  },
+  {
+    id: 4,
+    name: "UNC Campus Health",
+    description:
+      "Speak to a caring medical provider about your mental health needs and their impact on your physical health. Campus Health also provides medication management.",
+    website: "https://campushealth.unc.edu",
+    phoneNumber: "9199662281",
+    location: "320 Emergency Room Drive, Chapel Hill, NC 27599",
+    keywords: "Professional Care, Student Services",
+    image:
+      "https://campushealth.unc.edu/wp-content/uploads/2021/09/cropped-DSC0264-scaled-1.jpg",
+  },
+  {
+    id: 5,
+    name: "CAPS 24/7 Phone Line",
+    description:
+      "After hours, the CAPS phone number becomes a crisis support line for students. Call 919-966-3658 if you are in a crisis situation.",
+    website: "https://caps.unc.edu",
+    phoneNumber: "9199663658",
+    location: "320 Emergency Room Drive, Chapel Hill, NC, 27599",
+    keywords: "24/7, Student Services, Counseling",
+    image:
+      "https://studentsuccess2.web.unc.edu/wp-content/uploads/sites/10139/2015/10/CAPS-Logo.png",
+  },
+  {
+    id: 6,
+    name: "Suicide Prevention Hotline",
+    description:
+      "We can all help prevent suicide. The Lifeline provides 24/7, free and confidential support for people in distress, prevention and crisis resources for you or your loved ones, and best practices for professionals",
+    keywords: "Emergency, Counseling, Professional Care, 24/7",
+    location: "n/a",
+    website: "SuicidePreventionLifeline.org/",
+    image:
+      "https://suicidepreventionlifeline.org/wp-content/themes/lifeline/assets/icons/NSPL_Rv2_2.jpg",
+    phoneNumber: "1-800-273-8255",
+  },
+  {
+    id: 7,
+    name: "Triangle Neuropsychology Services ",
+    description:
+      "a Clinical Neuropsychology practice specializing in patient-centered care for people with concern about cognitive function.",
+    keywords: "Evaluation, Neuropsychology, Testing",
+    location: "3310 Croasdaile Drive, Suite 400 Durham, NC 27705",
+    website: "https://triangleneuropsychology.com",
+    phoneNumber: "919-384-9682",
+    image:
+      "https://triangleneuropsychology.com/wp-content/uploads/2020/11/Triangle-Neuro-Who-We-Work-With.jpg",
+  },
+  {
+    id: 8,
+    name: "Crisis Text Line",
+    description:
+      "Crisis Text Line is here for any crisis. A live, trained Crisis Counselor receives the text and responds, all from our secure online platform. The volunteer Crisis Counselor will help you move from a hot moment to a cool moment.",
+    keywords: "Text, 24/7, Crisis Support",
+    location: "n/a",
+    phoneNumber: "text HOME to 741741 ",
+    website: "https://www.crisistextline.org",
+    image:
+      "https://pbs.twimg.com/profile_images/1146468729749946369/UxPxbvoI_400x400.png",
+  },
+  {
+    id: 9,
+    name: "UNC Peer2Peer",
+    description:
+      "a student-led group that advocates for the mental well-being of the members within our community through peer support. We offer free one-to-one sessions to talk with a peer responder with the option of remaining anonymous.",
+    keywords: "Student Help, Peer Counseling, Anonymous",
+    location: "n/a",
+    phoneNumber: "n/a",
+    website: "https://www.uncpeer2peer.com",
+    image:
+      "https://www.uncpeer2peer.com/uploads/1/3/2/1/132179020/published/screen-shot-2021-01-19-at-9-15-05-am.png?1611336382",
+  },
+  {
+    id: 10,
+    name: "Compass Center",
+    description:
+      "Our services include career and financial education, domestic violence crisis and prevention programs, assistance with legal resources, and youth health programs",
+    keywords: "Women, Family, Resources, Domestic Violence",
+    location: "210 Henderson Street, Chapel Hill, NC 27514",
+    phoneNumber: "9199684610",
+    website: "https://www.compassctr.org/about",
+    image:
+      "https://bonner.unc.edu/wp-content/uploads/sites/300/2018/11/Compass-Center.jpg",
+  },
+];
